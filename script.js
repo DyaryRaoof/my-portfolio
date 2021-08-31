5933647deb36ddf35e595fff0c4d868f232a2931
mustabbas <42379733+mustabbas@users.noreply.github.com>
2021-09-01 00:47:10 +0300

    Fix desktop style and Add javascript function to popup detail window

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const portfolioItem = document.querySelector('#portfolio-item');
 const aboutItem = document.querySelector('#about-item');
 const contactItem = document.querySelector('#contact-item');
 
+const seeProjectButton = document.querySelectorAll('.see-project');
+const popupWrapper = document.querySelector('#popup-wrapper');
+const bodyWrapper = document.querySelector('.body-wrapper');
+const popupWindowCloseButton = document.querySelector('#popup-window-close-button');
 function displayMenu() {
   popupNav.classList.remove('display-none');
 }
@@ -13,8 +17,21 @@ function hideMenu() {
   popupNav.classList.add('display-none');
 }
 
+function togglePopup() {
+   popupWrapper.classList.toggle('display-none');
+   bodyWrapper.classList.toggle('opacity-main-content');
+}
+
 hamburger.addEventListener('click', displayMenu);
 closeButton.addEventListener('click', hideMenu);
 portfolioItem.addEventListener('click', hideMenu);
 aboutItem.addEventListener('click', hideMenu);
 contactItem.addEventListener('click', hideMenu);
+
+for (let i = 0; i < seeProjectButton.length; i++) {
+    seeProjectButton[i].addEventListener('click', function () {
+        togglePopup(i)
+    });
+}
+
+popupWindowCloseButton.addEventListener('click',togglePopup);
